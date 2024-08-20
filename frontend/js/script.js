a29eca383af62e625bf60fb0f1998d7d33201cb1
LuizSim <a_simao@icloud.com>
2024-08-19 21:55:41 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -81,7 +81,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("wss://chatsimao.onrender.com")
+    websocket = new WebSocket("ws://localhost:8080")
     websocket.onmessage = processMessage
 }
 
